4f80476f650f1eece20127ca5dbb6b8312161626
James Lyne <jim+github@not-null.co.uk>
2021-02-22 16:28:59 +0000

    Improve loot handling

--- src/main/java/uk/co/notnull/CustomItems/Listeners/Loot.java
+++ src/main/java/uk/co/notnull/CustomItems/Listeners/Loot.java
@@ -1,8 +1,5 @@
 package uk.co.notnull.CustomItems.Listeners;
 
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockState;
-import org.bukkit.block.Container;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -11,18 +8,13 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.event.player.PlayerFishEvent;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.LootGenerateEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import uk.co.notnull.CustomItems.CustomItems;
 import uk.co.notnull.CustomItems.ItemManager;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,44 +27,15 @@ public class Loot implements Listener {
         this.manager = plugin.getItemManager();
     }
 
-    //TODO: Remove this once betterend etc uses loot tables
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onInteract(PlayerInteractEvent event) {
-        Block block = event.getClickedBlock();
-
-        if(block == null) {
-            return;
-        }
-
-        BlockState state = block.getState();
-
-        if(!(state instanceof Container)) {
-            return;
-        }
-
-        handleInventory(((Container) block.getState()).getInventory(), event.getPlayer());
-    }
-
-    //TODO: Remove this once betterend etc uses loot tables
-    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
-    public void onInteractEntity(PlayerInteractEntityEvent event) {
-        Entity entity = event.getRightClicked();
-
-        if(!(entity instanceof InventoryHolder)) {
-            return;
-        }
-
-        handleInventory(((InventoryHolder) entity).getInventory(), event.getPlayer());
-    }
-
-    //TODO: Remove this once betterend etc uses loot tables
     @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
     public void onInventoryOpen(InventoryOpenEvent event) {
         Inventory inventory = event.getInventory();
 
-        if(inventory.getType() == InventoryType.ENDER_CHEST) {
-            handleInventory(inventory, (Player) event.getPlayer());
+        if(inventory.isEmpty()) {
+            return;
         }
+
+        handleInventory(inventory, (Player) event.getPlayer());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
