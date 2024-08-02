62ebee7396f4fb444320d976fcd402530a5077de
James Lyne <jim+github@not-null.co.uk>
2024-08-02 15:54:56 +0100

    Replace shadow with PluginLoader

---
+++ src/main/java/uk/co/notnull/CustomItems/CustomItemsLoader.java
@@ -0,0 +1,21 @@
+package uk.co.notnull.CustomItems;
+
+import io.papermc.paper.plugin.loader.PluginClasspathBuilder;
+import io.papermc.paper.plugin.loader.PluginLoader;
+import io.papermc.paper.plugin.loader.library.impl.MavenLibraryResolver;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.eclipse.aether.graph.Dependency;
+import org.eclipse.aether.repository.RemoteRepository;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings({"UnstableApiUsage", "unused"})
+public class CustomItemsLoader implements PluginLoader {
+	@Override
+	public void classloader(@NotNull PluginClasspathBuilder classpathBuilder) {
+		MavenLibraryResolver resolver = new MavenLibraryResolver();
+        resolver.addDependency(new Dependency(new DefaultArtifact("uk.co.notnull:MessagesHelper-paper:1.0-SNAPSHOT"), null));
+        resolver.addRepository(new RemoteRepository.Builder("notnull", "default", "https://repo.not-null.co.uk/snapshots/").build());
+
+        classpathBuilder.addLibrary(resolver);
+	}
+}
