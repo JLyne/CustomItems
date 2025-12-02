87b166b1a487d973990e2f585426657b26846658
James Lyne <jim+github@not-null.co.uk>
2025-12-02 15:30:51 +0000

    Allow collections to be returned from provideItems

--- CustomItemsAPI/src/main/java/uk/co/notnull/CustomItems/api/items/CustomItemProvider.java
+++ CustomItemsAPI/src/main/java/uk/co/notnull/CustomItems/api/items/CustomItemProvider.java
@@ -3,10 +3,10 @@ package uk.co.notnull.CustomItems.api.items;
 import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
+import java.util.Collection;
 
 @Deprecated(forRemoval = true)
 public abstract class CustomItemProvider implements uk.co.notnull.CustomItems.api.items.provider.CustomItemProvider {
-	public abstract List<CustomItem> provideItems();
+	public abstract Collection<CustomItem> provideItems();
 	public abstract @Nullable CustomItem identifyItem(ItemStack item);
 }
