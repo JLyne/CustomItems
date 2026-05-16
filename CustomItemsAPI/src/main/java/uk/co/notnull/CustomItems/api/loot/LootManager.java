f42fdcfd5275b0e9081745566d00ab1002799484
James Lyne <jim+github@not-null.co.uk>
2026-05-16 17:04:00 +0100

    Fix build

--- CustomItemsAPI/src/main/java/uk/co/notnull/CustomItems/api/loot/LootManager.java
+++ CustomItemsAPI/src/main/java/uk/co/notnull/CustomItems/api/loot/LootManager.java
@@ -2,8 +2,7 @@ package uk.co.notnull.CustomItems.api.loot;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-
-import javax.annotation.Nullable;
+import org.jspecify.annotations.Nullable;
 
 public interface LootManager {
 	@Nullable ItemStack generateLoot(String pool, Player player, int amount);
